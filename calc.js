2ae5cbd4ae1bcaf3383ada566fdf89ac53aa3230
sabeer <sabeerbikba@gmail.com>
2023-05-03 15:58:43 +0530

    adding files

---
+++ calc.js
@@ -0,0 +1,26 @@
+let buttons  = document.querySelectorAll('span');
+let input = document.querySelector('input')
+let string = '';
+Array.from(buttons);
+buttons.forEach( function(buttons) {
+    buttons.addEventListener('click', function(e) {
+        if(e.target.innerHTML == '=') {
+            string = eval(string);
+            input.value = string; 
+        } else if (e.target.innerHTML == 'AC') {
+            string = '';
+            input.value = string;
+        }
+        else {
+            console.log(e.target);
+            string = string + e.target.innerHTML;
+            input.value = string
+        }
+    })
+})
+
+// function func(event) {
+//     alert(event.target.nodeName);
+// }
+
+//target , eval 
